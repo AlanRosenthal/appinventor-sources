fde882bd294079d91683e9131763b072cc1521ff
Alan Rosenthal <alan@AlanECG.(none)>
2013-10-29 10:32:11 -0400

    scripts

---
+++ deply_server.sh
@@ -0,0 +1,1 @@
+../appengine-java-sdk-1.8.4/bin/appcfg.sh -A alanappinventor2 update appinventor/appengine/build/war/
---
+++ start_server.sh
@@ -0,0 +1,2 @@
+../appengine-java-sdk-1.8.4/bin/dev_appserver.sh --port=8888 --address=0.0.0.0 appinventor/appengine/build/war/ &
+ant appinventor/buildsever/RunLocalBuildServer &
